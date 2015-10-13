7d24406be29cd0967f42686c9c6d2fd1cb0a8f56
RangerMauve <RangerMauve@htomail.com>
2015-10-12 21:38:49 -0400

    Added tests and docs

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # promise-channel
-Like Go Channels but using JavaScript Promises.
+Like Go Channels, but using JavaScript Promises.
+
+```
+npm install --save promise-channel
+```
+
+## Example
+This example assumes a [babel](https://babeljs.io/) environment and uses [async functions](https://github.com/tc39/ecmascript-asyncawait). If you aren't using babel, this works well with generators wrapped by [co](https://www.npmjs.com/package/co). Reads and writes return promises, so you could use whatever flow control you want around that.
+
+This should look pretty familiar if you've used [goroutines](https://gobyexample.com/goroutines) with [go channels](https://tour.golang.org/concurrency/2);
+
+```JavaScript
+var makeChannel = require("promise-channel");
+
+var channel = makeChannel();
+
+Promise.all([
+    alice(channel),
+    bob(channel)
+]).then(function() {
+    channel.close();
+}).catch(function (e) {
+    console.error(e.stack);
+});
+
+async function alice(channel) {
+    var name = await channel();
+    await channel("Hello, " + name);
+}
+
+async function bob(channel) {
+    await channel("World");
+    var greeting = await channel();
+
+    console.log(greeting);
+}
+```
+
+## API
+### `make()`
+This is the default export from the module.
+
+#### return
+A new channel
+
+### `channel()`
+When you invoke the channel without any arguments (or with undefined), this is considered a read.
+
+#### return
+A promise which resolves to whatever value is written to the channel next.
+
+### `channel(value)`
+When you invoke the channel with an argument, the value is then passed to whatever reads from the channel next.
+
+#### arguments
+- `value` : The value to write to the channel
+
+#### return
+A promise which resolves once something has read from the channel.
+
+### `channel.close()`
+Closes the channel so that all pending and subsequent reads/writes will be rejected.
+
+### `channel.open`
+Property which tells you whether the channel is currently open
---
+++ test.js
@@ -0,0 +1,98 @@
+var test = require("blue-tape");
+var assertRejected = require("assert-rejected");
+var assert = require("assert");
+var make = require("./");
+
+test("Should be able to queue multiple reads", function(t) {
+	var chan = make();
+	chan();
+	chan();
+	t.end();
+});
+
+test("Should be able to queue multiple writes", function(t) {
+	var chan = make();
+	chan("Test");
+	chan("Test");
+	t.end();
+});
+
+test("Read should resolve after a write", function(t) {
+	var chan = make();
+	var read = chan();
+	chan("Test");
+	return read;
+});
+
+test("Reads should resolve to the value in the write", function(t) {
+	var chan = make();
+	var read = chan();
+	chan("Test");
+	return read.then(function(value) {
+		assert.equal(value, "Test", "Result of read didn't match");
+	});
+});
+
+test("Read should resolve if write was already queued", function(t) {
+	var chan = make();
+	chan("Test");
+	var read = chan();
+	return read.then(function(value) {
+		assert.equal(value, "Test", "Result of read didn't match");
+	});
+});
+
+
+test("Write should resolve after a read", function(t) {
+	var chan = make();
+	var write = chan("Test");
+	var read = chan();
+	return write;
+});
+
+test("Channel should be marked as open before close", function(t) {
+	var chan = make();
+	assert(chan.open, "Channel looks not open");
+	t.end();
+});
+
+test("Should be able to close a channel", function(t) {
+	var chan = make();
+	chan.close();
+	t.end();
+});
+
+test("Channel should be marked as not open after close", function(t) {
+	var chan = make();
+	chan.close();
+	assert(!chan.open, "Channel looks open");
+	t.end();
+});
+
+test("Pending reads should reject after close", function(t) {
+	var chan = make();
+	var read = chan();
+	chan.close();
+	return assertRejected(read);
+});
+
+test("Pending writes should reject after close", function(t) {
+	var chan = make();
+	var write = chan("Test");
+	chan.close();
+	return assertRejected(write);
+});
+
+test("Subsequent reads should reject after close", function(t) {
+	var chan = make();
+	chan.close();
+	var read = chan();
+	return assertRejected(read);
+});
+
+test("Subsequent writes should reject after close", function(t) {
+	var chan = make();
+	chan.close();
+	var write = chan("Test");
+	return assertRejected(write);
+});
